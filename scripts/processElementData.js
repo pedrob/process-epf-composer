f02258e7994cdc122ac80c418a8f06a040bfc9d7
PEDRO BEETHOVEN DA COSTA <pedro.beethoven@academico.ifrn.edu.br>
2019-11-28 18:37:57 -0300

    Add static site from process

---
+++ scripts/processElementData.js
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_PZDPURHYEeq4nb-MIjkIBwActivity"]="./../../Design Sprint/deliveryprocesses/resources/design_sprint_9BE1CAEE_9be1caee_Activity.jpeg"
+contentPage.processPage.elementUrls["_UIuDcBHYEeq4nb-MIjkIBw"] = ["Bussiness Role","Design Sprint/deliveryprocesses/bussiness_role_CDB0133D.html"];
+contentPage.processPage.elementUrls["_UIuDdBHYEeq4nb-MIjkIBw"] = ["Designer","Design Sprint/deliveryprocesses/designer_9241F55C.html"];
+contentPage.processPage.elementUrls["_UIuDeBHYEeq4nb-MIjkIBw"] = ["Facilitator","Design Sprint/deliveryprocesses/facilitator_56D3D77B.html"];
+contentPage.processPage.elementUrls["_UIlgkBHYEeq4nb-MIjkIBw"] = ["Sketch","Design Sprint/deliveryprocesses/sketch_EBD9F841.html"];
+contentPage.processPage.elementUrls["_UIuDchHYEeq4nb-MIjkIBw"] = ["Decision Maker","Design Sprint/deliveryprocesses/decision_maker_C6E70763.html"];
+contentPage.processPage.elementUrls["_wT70oBHbEeq4nb-MIjkIBw"] = ["Prototype","Design Sprint/deliveryprocesses/prototype_6B11308D.html"];
+contentPage.processPage.elementUrls["_ttTwohHbEeq4nb-MIjkIBw"] = ["Detailed Sprint Notes","Design Sprint/deliveryprocesses/detailed_sprint_notes_AB6C0A40.html"];
+contentPage.processPage.elementUrls["_PZDPURHYEeq4nb-MIjkIBw"] = ["Design Sprint","Design Sprint/deliveryprocesses/design_sprint_9BE1CAEE.html"];
+contentPage.processPage.elementUrls["_UiXSgRHYEeq4nb-MIjkIBw"] = ["Sprint Notes","Design Sprint/deliveryprocesses/sprint_notes_4D5A9D75.html"];
+contentPage.processPage.elementUrls["_wzISEBHbEeq4nb-MIjkIBw"] = ["Test","Design Sprint/deliveryprocesses/test_7182F8AE.html"];
+contentPage.processPage.elementUrls["_UIuDdhHYEeq4nb-MIjkIBw"] = ["Engineer","Design Sprint/deliveryprocesses/engineer_8B78E982.html"];
+contentPage.processPage.elementUrls["_UIuDehHYEeq4nb-MIjkIBw"] = ["Product Manager","Design Sprint/deliveryprocesses/product_manager_500ACBA1.html"];
+contentPage.processPage.elementUrls["_ttTwoBHbEeq4nb-MIjkIBw"] = ["Capture Document","Design Sprint/deliveryprocesses/capture_document_B235161A.html"];
+contentPage.processPage.elementUrls["_wXNyIBHbEeq4nb-MIjkIBw"] = ["Prototype","Design Sprint/deliveryprocesses/prototype_E4ED9889.html"];
+contentPage.processPage.elementUrls["_tpzwsBHbEeq4nb-MIjkIBw"] = ["Decide","Design Sprint/deliveryprocesses/decide_F41E9AAC.html"];
+contentPage.processPage.elementUrls["_UiXSgBHYEeq4nb-MIjkIBw"] = ["Unpack","Design Sprint/deliveryprocesses/unpack_42BCBD65.html"];
